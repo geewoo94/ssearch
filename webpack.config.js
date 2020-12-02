e4689d5e114ee912f0fab93accef1291ec5b7aae
gyeongwoopark <geewoo94@gmail.com>
2020-12-02 14:49:58 +0900

    🔧 initial webpack config

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  devServer: {
+    contentBase: __dirname + "/dist/",
+    inline: true,
+    hot: true,
+    host: "localhost",
+    port: 3000
+  },
+  entry: [
+    './src/index.js',
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|ico)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              useRelativePath: true,
+              limit: 10000
+            }
+          }
+        ]
+      },
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new HtmlWebpackPlugin({
+      minify: false,
+    }),
+  ],
+};

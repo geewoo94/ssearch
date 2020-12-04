ec1411975d4aad4f89519e9bafafd81b596822c6
gyeongwoopark <geewoo94@gmail.com>
2020-12-04 21:52:42 +0900

    💄 update css style

--- src/Components/Header.ts
+++ src/Components/Header.ts
@@ -13,13 +13,17 @@ const style = `
 function Header({ range, setRange }: { range: string, setRange: Function }): Function {
   return render(
     Div({ class: 'Header-Wrapper' })(
-      Img({ src: './main-icon-128.png' })(),
-      Input({ value: '검색!' })(),
+      Div({ class: 'Right-Column' })(),
+      Div({ class: 'Header-Column' })(
+        Img({ src: './main-icon-128.png' })(),
+        Input({ placeholder: '검색을 껌색하세요!', class: 'Search-Input' })(),
+      ),
       Input({
         type: 'range',
         min: '0',
         max: '7',
         value: range,
+        class: 'Range-Input',
         onChange: (ev: any) => setRange(ev.target.value),
       })(),
     )
